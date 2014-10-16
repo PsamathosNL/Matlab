460947953d02082bf3b740162416d75524e8b1f3
Jaap <jaap_1993@msn.com>
2014-10-16 21:50:11 +0200

    Added input checking. Currently only surporting height, width and filename. More comming.

--- ImageProcessing/Process.m
+++ ImageProcessing/Process.m
@@ -1,4 +1,4 @@
-function z = Process(Width, Height, filename)
+function z = Process(filename, varargin)
 % The function Process is designed to create neat pictures for use in
 % reports. It saves a pdf in a (newly created) folder called figures.
 % Figures are stored in pdf.
@@ -7,38 +7,37 @@ function z = Process(Width, Height, filename)
 % Height is image height relative to page with in percentage
 % Name is a string containing the inteded image name.
 
+p = inputParser;
+p.CaseSensitive = false;
+defaultHeight = 100;
+defaultWidth = 161;
+defaultTitle = 'Awesome plot by Jaap';
+addOptional(p, 'width',  defaultWidth, @isnumeric);
+addOptional(p, 'height', defaultHeight, @isnumeric);
+addOptional(p, 'title', defaultTitle);
+%  addParameter(p,'shape',defaultShape,...
+%                 @(x) any(validatestring(x,expectedShapes)));
+parse(p, filename, varargin{:})
+
 if exist('figures/fig', 'dir') ~= 7   %checks if apropriate folder exists
-    str = strcat('figures', filesep, 'fig');
-    mkdir(str);
+    mkdir(strcat('figures', filesep, 'fig'));
 end
 saveas(gcf, fullfile('figures', filesep, 'fig',filesep, filename), 'fig'); %saving backup fig
 
+
 %% Initializing defaults
-page = 24; %defailt a4 width
-FontSize = 10;
-GraphLineWidth = 3;
-MarkerSize = 10; % If a marker was used (x,+,*,o...) This is it's size.
+FontSize = 24;           % Used fontsize (labels, legenda, etc)
+GraphLineWidth = 3;      % The linewidth of the graph lines
+MarkerSize = 10;         % The size of the markers
 
-FontName = 'AvantGarde'; % Fonttype, chosen freely.
-FontWeight = 'Bold';  % [light normal, demi, bold]
+FontName = 'Helvetica';   % Fonttype, chosen semi freely (only a couple are supported)
+FontWeight = 'Bold';       % [light normal, demi, bold]
 
-%% Changing figure borders and grid, grid might be offensive
-grid off % Turns the grid off (prefered by most editors)
-box on  % Turns box on
-GridLineStyle = '--'; % [-, --, :, -., none]
-LineWidth = 1; % Thickness of the grid and the axis.
-
-%% Gathering input
-if nargin == 0
-    Height = 50;
-    Width = 60;
-    Name = get(get(gca, 'Title'), 'String'); % Title of the graph, figure is saved with this name.
-elseif nargin == 2
-    Name = get(get(gca, 'Title'), 'String'); % Title of the graph, figure is saved with this name.
-end
-
-Height = Height * page / 100;
-Width  = Width  * page / 100;
+%% Changing figure borders and grid
+grid off                % Turns the grid off (prefered by most editors)
+box on                  % Turns box on
+GridLineStyle = '--';   % [-, --, :, -., none] not in use when grid is off
+LineWidth = 1;          % Thickness of the axis and grid.
 
 %Settings are applied from here, to adjust the layout of the graph only
 %change the upper part.
@@ -46,8 +45,10 @@ Width  = Width  * page / 100;
 hline = findobj(gcf, 'type', 'line');
 set(hline, 'LineWidth', GraphLineWidth, 'MarkerSize', MarkerSize)
 set(gcf, 'PaperUnits', 'centimeters')
-set(gcf, 'PaperPosition', [0, 0, Width, Height]', 'PaperSize', [Width, Height]);
-set(gca, 'LineWidth', LineWidth, 'GridLineStyle', GridLineStyle, 'FontSize', FontSize, 'fontWeight', FontWeight, 'FontName', FontName)
+set(gcf, 'PaperPosition', [0, 0, p.Results.width, p.Results.height]',...
+         'PaperSize', [p.Results.width, p.Results.height]);
+set(gca, 'LineWidth', LineWidth, 'GridLineStyle', GridLineStyle,...
+         'FontSize', FontSize, 'fontWeight', FontWeight, 'FontName', FontName)
 h_xlabel = get(gca,'XLabel');
 h_ylabel = get(gca,'YLabel');
 h_title = get(gca, 'Title');
