b3cd346b90310e6dba60851e961e7f6012a49318
Jaap <jaap_1993@msn.com>
2014-10-16 22:17:36 +0200

    Added help text

--- ImageProcessing/Process.m
+++ ImageProcessing/Process.m
@@ -1,32 +1,50 @@
 function z = Process(filename, varargin)
-% The function Process is designed to create neat pictures for use in
-% reports. It saves a pdf in a (newly created) folder called figures.
-% Figures are stored in pdf.
-% Input commands are:
-% Width is image width relative to page width in percentage
-% Height is image height relative to page with in percentage
-% Name is a string containing the inteded image name.
+% Process() is a function designed to make consistent image processing
+% easy and nice. It should be used by calling it after plotting an image.
+% Required additions as labels, legenda etc should be added before calling
+% this function. When ready Process can be called. The function creates
+% two images. A .fig file as backup when the layout of images is not
+% fullfilling and a .pdf file which can be used in reports. A folder 
+% "figures" is created in which the pdf-files are stored. It contains a
+% subfolder fig which contains the fig-files. 
+
+%
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% %%%%                            Usage                                %%%
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Process needs no input arguments although it is probably neccesary to add
+% some. There are various chaging parameters which can be changed using the
+% flags:
+% title     -   The filename under which the images are stored
+% height    -   The image height
+% widht     -   The image widht
+% fontsize  -   The fontsize of labels, legenda entries etc. 
+% Example:
+% Process('title', 'Excellent plot', 'height', 100, 'width', 161)
+
 
 p = inputParser;
 p.CaseSensitive = false;
 defaultHeight = 100;
 defaultWidth = 161;
+defaultFontSize = 24;           % Used fontsize (labels, legenda, etc)
 defaultTitle = 'Awesome plot by Jaap';
 addOptional(p, 'width',  defaultWidth, @isnumeric);
 addOptional(p, 'height', defaultHeight, @isnumeric);
-addOptional(p, 'title', defaultTitle);
-%  addParameter(p,'shape',defaultShape,...
-%                 @(x) any(validatestring(x,expectedShapes)));
+addOptional(p, 'fontsize', defaultFontSize, @isnumeric);
+addOptional(p, 'filename', defaultTitle);
+
 parse(p, filename, varargin{:})
 
 if exist('figures/fig', 'dir') ~= 7   %checks if apropriate folder exists
     mkdir(strcat('figures', filesep, 'fig'));
 end
-saveas(gcf, fullfile('figures', filesep, 'fig',filesep, filename), 'fig'); %saving backup fig
+saveas(gcf, fullfile('figures', filesep,...
+            'fig',filesep, filename), 'fig'); %saving backup fig
 
 
 %% Initializing defaults
-FontSize = 24;           % Used fontsize (labels, legenda, etc)
 GraphLineWidth = 3;      % The linewidth of the graph lines
 MarkerSize = 10;         % The size of the markers
 
@@ -48,13 +66,17 @@ set(gcf, 'PaperUnits', 'centimeters')
 set(gcf, 'PaperPosition', [0, 0, p.Results.width, p.Results.height]',...
          'PaperSize', [p.Results.width, p.Results.height]);
 set(gca, 'LineWidth', LineWidth, 'GridLineStyle', GridLineStyle,...
-         'FontSize', FontSize, 'fontWeight', FontWeight, 'FontName', FontName)
+         'FontSize', p.Results.defaultFontSize, 'fontWeight', FontWeight,...
+         'FontName', FontName)
 h_xlabel = get(gca,'XLabel');
 h_ylabel = get(gca,'YLabel');
 h_title = get(gca, 'Title');
-set(h_xlabel,'FontSize', FontSize, 'FontWeight', FontWeight, 'FontName', FontName);
-set(h_ylabel,'FontSize', FontSize, 'FontWeight', FontWeight, 'FontName', FontName);
-set(h_title,'FontSize', FontSize+3, 'FontWeight', FontWeight, 'FontName', FontName);
+set(h_xlabel,'FontSize', p.Results.defaultFontSize,...
+             'FontWeight', FontWeight, 'FontName', FontName);
+set(h_ylabel,'FontSize', p.Results.defaultFontSize,...
+             'FontWeight', FontWeight, 'FontName', FontName);
+set(h_title,'FontSize', p.Results.defaultFontSize+3,...
+            'FontWeight', FontWeight, 'FontName', FontName);
 
 saveas(gcf, fullfile('figures', filename), 'pdf') %Saves the image as pdf in the figures folder.
 end
