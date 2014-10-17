fdfec59d39c7a6148c20e58c2607ce58a4101de7
Jaap <jaap_1993@msn.com>
2014-10-17 22:07:13 +0200

    Added beter input parsing. Adjusted help file. Script can now auto-cut the image.

--- ImageProcessing/Process.m
+++ ImageProcessing/Process.m
@@ -4,36 +4,38 @@ function z = Process(filename, varargin)
 % Required additions as labels, legenda etc should be added before calling
 % this function. When ready Process can be called. The function creates
 % two images. A .fig file as backup when the layout of images is not
-% fullfilling and a .pdf file which can be used in reports. A folder 
+% fullfilling and a .pdf file which can be used in reports. A folder
 % "figures" is created in which the pdf-files are stored. It contains a
-% subfolder fig which contains the fig-files. 
-
+% subfolder fig which contains the fig-files.
+%
 %
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% %%%%                            Usage                                %%%
+% %%%                             Usage                                %%%
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
+%
 % Process needs no input arguments although it is probably neccesary to add
 % some. There are various chaging parameters which can be changed using the
 % flags:
 % title     -   The filename under which the images are stored
 % height    -   The image height
 % widht     -   The image widht
-% fontsize  -   The fontsize of labels, legenda entries etc. 
+% fontsize  -   The fontsize of labels, legenda entries etc.
 % Example:
 % Process('title', 'Excellent plot', 'height', 100, 'width', 161)
 
 
 p = inputParser;
-p.CaseSensitive = false;
-defaultHeight = 100;
-defaultWidth = 161;
+p.CaseSensitive = false;        % Fuck capitals
+defaultHeight = 100;            % Default image height
+defaultWidth = 161;             % Default image width (based on phi)
 defaultFontSize = 24;           % Used fontsize (labels, legenda, etc)
+defaultAutoCut = false;
 defaultTitle = 'Awesome plot by Jaap';
 addOptional(p, 'width',  defaultWidth, @isnumeric);
 addOptional(p, 'height', defaultHeight, @isnumeric);
 addOptional(p, 'fontsize', defaultFontSize, @isnumeric);
 addOptional(p, 'filename', defaultTitle);
+addOptional(p, 'autoCut', defaultAutoCut); %add boolean check
 
 parse(p, filename, varargin{:})
 
@@ -41,7 +43,7 @@ if exist('figures/fig', 'dir') ~= 7   %checks if apropriate folder exists
     mkdir(strcat('figures', filesep, 'fig'));
 end
 saveas(gcf, fullfile('figures', filesep,...
-            'fig',filesep, filename), 'fig'); %saving backup fig
+    'fig',filesep, filename), 'fig'); %saving backup fig
 
 
 %% Initializing defaults
@@ -50,6 +52,8 @@ MarkerSize = 10;         % The size of the markers
 
 FontName = 'Helvetica';   % Fonttype, chosen semi freely (only a couple are supported)
 FontWeight = 'Bold';       % [light normal, demi, bold]
+fontColor = 0.4*ones(1,3);
+axisColor = 0.5*ones(1,3);
 
 %% Changing figure borders and grid
 grid off                % Turns the grid off (prefered by most editors)
@@ -60,23 +64,36 @@ LineWidth = 1;          % Thickness of the axis and grid.
 %Settings are applied from here, to adjust the layout of the graph only
 %change the upper part.
 
-hline = findobj(gcf, 'type', 'line');
-set(hline, 'LineWidth', GraphLineWidth, 'MarkerSize', MarkerSize)
+h_line = findobj(gcf, 'type', 'line');
+
+if p.Results.autoCut
+    xmin = min([h_line.XData]);
+    ymin = min([h_line.YData]);
+    xmax = max([h_line.XData]);
+    ymax = max([h_line.YData]);
+    cutoffX = [xmin-0.1*(xmax-xmin), xmax + 0.1*(xmax-xmin)];
+    cutoffY = [ymin-0.1*(ymax-ymin), ymax + 0.1*(ymax-ymin)];
+    
+    axis([cutoffX, cutoffY])
+end
+
+set(h_line, 'LineWidth', GraphLineWidth, 'MarkerSize', MarkerSize)
 set(gcf, 'PaperUnits', 'centimeters')
 set(gcf, 'PaperPosition', [0, 0, p.Results.width, p.Results.height]',...
-         'PaperSize', [p.Results.width, p.Results.height]);
+    'PaperSize', [p.Results.width, p.Results.height]);
 set(gca, 'LineWidth', LineWidth, 'GridLineStyle', GridLineStyle,...
-         'FontSize', p.Results.defaultFontSize, 'fontWeight', FontWeight,...
-         'FontName', FontName)
+    'FontSize', p.Results.fontsize, 'fontWeight', FontWeight,...
+    'FontName', FontName,...
+    'XColor', axisColor, 'YColor', axisColor)
+
 h_xlabel = get(gca,'XLabel');
 h_ylabel = get(gca,'YLabel');
 h_title = get(gca, 'Title');
-set(h_xlabel,'FontSize', p.Results.defaultFontSize,...
-             'FontWeight', FontWeight, 'FontName', FontName);
-set(h_ylabel,'FontSize', p.Results.defaultFontSize,...
-             'FontWeight', FontWeight, 'FontName', FontName);
-set(h_title,'FontSize', p.Results.defaultFontSize+3,...
-            'FontWeight', FontWeight, 'FontName', FontName);
+
+set([h_xlabel, h_ylabel, h_title],'FontSize', p.Results.fontsize,...
+    'FontWeight', FontWeight, 'FontName', FontName,...
+    'Color', fontColor);
+set(h_title,'FontSize', p.Results.fontsize+3)
 
 saveas(gcf, fullfile('figures', filename), 'pdf') %Saves the image as pdf in the figures folder.
 end
